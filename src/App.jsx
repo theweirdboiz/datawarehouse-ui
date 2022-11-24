3b7694cacd082cefd5a717d4549dafec10f28373
theweirdboiz <19130227@st.hcmuaf.edu.vn>
2022-11-24 23:14:24 +0700

    add loader

--- src/App.jsx
+++ src/App.jsx
@@ -21,87 +21,104 @@ import Search from "./components/Content/Search";
 import CardProp from "./components/Content/CardProp";
 import CardWidget from "./components/Content/CardWidget";
 import Widget from "./components/widget/Widget";
+import { useEffect, useState } from "react";
+import Loader from "./components/Loader";
 
 function App() {
-  const date = "font-[900] text-lg";
-  const detailDate = "text-sm text-[#888]";
+  const [load, setLoad] = useState(true);
+  useEffect(() => {
+    const timer = setTimeout(() => {
+      setLoad(false);
+    }, 800);
+    return () => {
+      clearTimeout(timer);
+    };
+  }, []);
+  const date = "font-[900] text-[32px]";
+  const detailDate = "text-[24px] text-[#888]";
 
-  const contentTop = "mt-[20px] flex items-center gap-x-10 mb-20";
+  const contentTop = "flex items-center gap-x-10 mb-20";
 
   return (
-    <div className="p-5 mx-auto max-w-7xl bg-[#fff]">
-      <div className="flex justify-between">
-        <section className="content max-w-3xl text-primary">
-          <div className={contentTop}>
-            <div className="flex-col">
-              <h3 className={date}>January 2022</h3>
-              <div className={detailDate}>Thursday, 11:11 AM</div>
-            </div>
-            <Search></Search>
-          </div>
-          <div className="content__detail mb-20">
-            <div className="flex items-center justify-between">
-              <h4 className="font-semibold">Today overview</h4>
+    <>
+      {load && <Loader />}
+      <div className="p-5 my-3 mx-auto max-w-7xl bg-[#fff] rounded-xl shadow-md app">
+        <div className="flex justify-between">
+          <section className="content max-w-3xl text-primary">
+            <div className={contentTop}>
               <div className="">
-                <button className="text-sm font-medium text-linkColor">
-                  More detail
-                </button>
+                <h3 className={date}>January 2022</h3>
+                <div className={detailDate}>
+                  <span>Thursday</span>,{" "}
+                  <span className="text-[22px] text-primary">11:11 AM</span>
+                </div>
+              </div>
+              <Search></Search>
+            </div>
+            <div className="content__detail mb-20">
+              <div className="flex items-center justify-between">
+                <h4 className="font-semibold">Today overview</h4>
+                <div className="">
+                  <button className="text-sm font-medium text-linkColor">
+                    More detail
+                  </button>
+                </div>
+              </div>
+              <div>
+                <div className="content__detail-box grid grid-cols-3 gap-4 mt-6">
+                  <CardProp />
+                  <CardProp />
+                  <CardProp />
+                  <CardProp />
+                  <CardProp />
+                  <CardProp />
+                </div>
               </div>
             </div>
-            <div>
-              <div className="content__detail-box grid grid-cols-3 gap-4 mt-6">
-                <CardProp />
-                <CardProp />
-                <CardProp />
-                <CardProp />
-                <CardProp />
-                <CardProp />
+            <div className="content__detail mb-20">
+              <div className="flex items-center justify-between">
+                <h4 className="font-semibold">Weather 24h</h4>
+                <div className="">
+                  <button className="text-sm font-medium text-linkColor">
+                    24h
+                  </button>
+                </div>
               </div>
+              <Swiper
+                slidesPerView={4}
+                spaceBetween={30}
+                pagination={{
+                  clickable: true,
+                }}
+                className="mySwiper"
+              >
+                <SwiperSlide>
+                  <CardWidget time="Hiện tại"></CardWidget>
+                </SwiperSlide>
+                <SwiperSlide>
+                  <CardWidget time="12:30"></CardWidget>
+                </SwiperSlide>
+                <SwiperSlide>
+                  <CardWidget time="Hiện tại"></CardWidget>
+                </SwiperSlide>
+                <SwiperSlide>
+                  <CardWidget time="12:30"></CardWidget>
+                </SwiperSlide>
+                <SwiperSlide>
+                  <CardWidget time="Hiện tại"></CardWidget>
+                </SwiperSlide>
+                <SwiperSlide>
+                  <CardWidget time="12:30"></CardWidget>
+                </SwiperSlide>
+              </Swiper>
             </div>
-          </div>
-          <div className="content__detail mb-20">
-            <div className="flex items-center justify-between">
-              <h4 className="font-semibold">Weather 24h</h4>
-              <div className="">
-                <button className="text-sm font-medium text-linkColor">
-                  24h
-                </button>
-              </div>
-            </div>
-            <Swiper
-              slidesPerView={4}
-              spaceBetween={30}
-              pagination={{
-                clickable: true,
-              }}
-              className="mySwiper"
-            >
-              <SwiperSlide>
-                <CardWidget time="Hiện tại"></CardWidget>
-              </SwiperSlide>
-              <SwiperSlide>
-                <CardWidget time="12:30"></CardWidget>
-              </SwiperSlide>
-              <SwiperSlide>
-                <CardWidget time="Hiện tại"></CardWidget>
-              </SwiperSlide>
-              <SwiperSlide>
-                <CardWidget time="12:30"></CardWidget>
-              </SwiperSlide>
-              <SwiperSlide>
-                <CardWidget time="Hiện tại"></CardWidget>
-              </SwiperSlide>
-              <SwiperSlide>
-                <CardWidget time="12:30"></CardWidget>
-              </SwiperSlide>
-            </Swiper>
-          </div>
-        </section>
-        <section className="content__right text-white min-w-[320px]">
-          <Widget></Widget>
-        </section>
+          </section>
+          <section className="content__right text-white min-w-[320px]">
+            <Widget></Widget>
+          </section>
+        </div>
       </div>
-    </div>
+    </>
   );
 }
 
---
+++ src/components/Loader.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Loader = () => {
+  return (
+    <div id="loader" className="loading">
+      <span className="loader"></span>
+    </div>
+  );
+};
+
+export default Loader;
