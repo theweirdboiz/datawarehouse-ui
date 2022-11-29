39891c5138a538b54ba6b84e85b48a96bbdc552f
light1406 <19130022@st.hcmuaf.edu.vn>
2022-11-29 14:15:00 +0700

    insert code redux

--- src/components/Content/CardProp.jsx
+++ src/components/Content/CardProp.jsx
@@ -4,20 +4,24 @@ import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import {
   faTemperatureEmpty,
   faCaretDown,
+  faDroplet,
 } from "@fortawesome/free-solid-svg-icons";
 
-const CardProp = () => {
+const CardProp = (props) => {
   return (
-    <div className="content__detail-item p-5 flex items-center gap-x-6 bg-backgroundSecondary shadow-lg rounded-xl">
-      <FontAwesomeIcon className="" icon={faTemperatureEmpty} />
-      <div className="speed flex flex-col">
-        <h4 className="text-[#888]">Thấp/Cao</h4>
-        <span className="text-[24px] font-semibold">24/27</span>
+    <div className="weather-card hover:scale-110 cursor-pointer duration-700 shadow-lg mt-6" onClick={props.handleOnclick}>
+      <h1 className="text-center text-xl font-bold">{props.provinceName}</h1>
+      <div className="content__detail-item p-5 flex items-center gap-x-6 bg-backgroundSecondary rounded-xl">
+        <FontAwesomeIcon className="text-[#df4848]" icon={faTemperatureEmpty} />
+        <div className="speed flex flex-col">
+          <h4 className="text-[#888]">Thấp/Cao</h4>
+          <span className="text-[24px] font-semibold">{props.lowestTempurature}/{props.highestTempurature}</span>
+        </div>
+        <h6 className="text-[#888]">
+          <FontAwesomeIcon className="text-[#3683ff]" icon={faDroplet} />
+          <span className="ml-2">{props.humidity}%</span>
+        </h6>
       </div>
-      <h6 className="text-[#888]">
-        <FontAwesomeIcon className="text-[#df4848]" icon={faCaretDown} />
-        <span className="ml-2">20%</span>
-      </h6>
     </div>
   );
 };
--- src/components/widget/Widget.jsx
+++ src/components/widget/Widget.jsx
@@ -1,82 +1,101 @@
-import React from "react";
+import React, { useEffect } from "react";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-
 import {
   faLocationDot,
   faSun,
   faTemperatureEmpty,
   faDroplet,
-  faEyeSlash,
   faGlassWaterDroplet,
   faWind,
-  faUmbrellaBeach,
+  faEye,
 } from "@fortawesome/free-solid-svg-icons";
+import { useDispatch, useSelector } from "react-redux";
 
-import WidgetProp from "./WidgetProp";
-const Widget = () => {
+const Widget = (props) => {
+  const weather = useSelector((state) => state.weather.weather);
+
+  const bgAirQuanlity = {
+    "Tốt": "bg-green-600",
+    "Khá": "bg-amber-400",
+    "Trung bình": "bg-orange-700",
+    "Kém": "bg-red-700",
+    "Rất kém": "bg-fuchsia-700"
+  }
+
   return (
-    <div className="bg-backgroundPrimary p-5 h-full rounded-xl shadow-2xl">
-      <div className="flex flex-col gap-x-12 justify-between mt-2 mb-5 ">
-        <div className="flex flex-col">
-          <h3 className="flex items-baseline">
-            <FontAwesomeIcon icon={faLocationDot} />
-            <span className="ml-2 text-xl ">Binh Duong</span>
-          </h3>
-          <span className="text-sm">Đã cập nhật 2 phút trước</span>
-        </div>
-        <div className="flex">
-          <img src="https://data.thoitiet.vn/weather/icons/04n@2x.png" alt="" />
-          <div className="flex items-center">
-            <h4 className="text-[64px]">25°</h4>
+    <div className="bg-backgroundPrimary p-5 rounded-xl shadow-2xl fixed min-w-[400px] min-h-[400px]">
+      {weather && (
+        <>
+          <div className="flex flex-col gap-x-12 justify-between mt-2 mb-5 ">
             <div className="flex flex-col">
-              <h5 className="text-[20px]">C</h5>
-              <h5 className="text-[20px]">F</h5>
+              <h3 className="flex items-baseline">
+                <FontAwesomeIcon icon={faLocationDot} />
+                <span className="ml-2 text-xl ">{weather.provinceName}</span>
+              </h3>
+            </div>
+            <div className="flex">
+              <img
+                src="https://data.thoitiet.vn/weather/icons/04n@2x.png"
+                alt=""
+              />
+              <div className="flex items-center">
+                <h4 className="text-[64px]">{weather.currentTemperature}°</h4>
+                <div className="flex flex-col">
+                  <h5 className="text-[20px]">C</h5>
+                  <h5 className="text-[20px]">F</h5>
+                </div>
+              </div>
             </div>
           </div>
-        </div>
-      </div>
-      <h3 className="font-semibold mb-6">Mây cụm</h3>
-      <div className="mb-6 flex justify-between items-center">
-        <h3 className="font-semibold">Chất lượng không khí:</h3>
-        <h3 className=" bg-red-700 px-4 py-1 rounded-lg">Kém</h3>
-      </div>
-      <div className="w-full border-b-[1px] mb-6 border-borderPrimary"></div>
-      <h3 className="font-semibold">Dự báo thời tiết</h3>
-      <WidgetProp
-        name="Độ ẩm"
-        icon={faTemperatureEmpty}
-        values="88%"
-      ></WidgetProp>
-      <WidgetProp
-        name="Độ ẩm"
-        icon={faTemperatureEmpty}
-        values="88%"
-      ></WidgetProp>
-      <WidgetProp
-        name="Độ ẩm"
-        icon={faTemperatureEmpty}
-        values="88%"
-      ></WidgetProp>{" "}
-      <WidgetProp
-        name="Độ ẩm"
-        icon={faTemperatureEmpty}
-        values="88%"
-      ></WidgetProp>{" "}
-      <WidgetProp
-        name="Độ ẩm"
-        icon={faTemperatureEmpty}
-        values="88%"
-      ></WidgetProp>{" "}
-      <WidgetProp
-        name="Độ ẩm"
-        icon={faTemperatureEmpty}
-        values="88%"
-      ></WidgetProp>{" "}
-      <WidgetProp
-        name="Độ ẩm"
-        icon={faTemperatureEmpty}
-        values="88%"
-      ></WidgetProp>
+          <h3 className="font-semibold mb-6">{weather.overview}</h3>
+          <div className="flex mt-1">
+            <div>
+              <FontAwesomeIcon icon={faTemperatureEmpty}/>
+              <span className="ml-2 font-bold">Thấp/Cao:</span>
+            </div>
+            <div className="value ml-2 text-[#d9d9d9]">{weather.lowestTemperature}/{weather.highestTemperature}</div>
+          </div>
+          <div className="flex mt-1">
+            <div>
+              <FontAwesomeIcon icon={faDroplet}/>
+              <span className="ml-2 font-bold">Độ ẩm:</span>
+            </div>
+            <div className="value ml-2 text-[#d9d9d9]">{weather.humidity*100}%</div>
+          </div>
+          <div className="flex  mt-1">
+            <div>
+              <FontAwesomeIcon icon={faEye}/>
+              <span className="ml-2 font-bold">Tầm nhìn:</span>
+            </div>
+            <div className="value ml-2 text-[#d9d9d9]">{weather.vision} km</div>
+          </div>
+          <div className="flex  mt-1">
+            <div>
+              <FontAwesomeIcon icon={faWind}/>
+              <span className="ml-2 font-bold">Gió:</span>
+            </div>
+            <div className="value ml-2 text-[#d9d9d9]">{weather.wind} km/giờ</div>
+          </div>
+          <div className="flex  mt-1">
+            <div>
+              <FontAwesomeIcon icon={faGlassWaterDroplet}/>
+              <span className="ml-2 font-bold">Điểm ngưng:</span>
+            </div>
+            <div className="value ml-2 text-[#d9d9d9]">{weather.stopPoint} °C</div>
+          </div>
+          <div className="flex mt-1">
+            <div>
+              <FontAwesomeIcon icon={faSun}/>
+              <span className="ml-2 font-bold">Chỉ số uv:</span>
+            </div>
+            <div className="value ml-2 text-[#d9d9d9]">{weather.uvIndex}</div>
+          </div>
+          <div className="mb-6 flex justify-between items-center mt-3">
+            <h3 className="font-semibold">Chất lượng không khí:</h3>
+            <h3 className= "px-4 py-1 rounded-lg ml-2">{weather.airQuality}</h3>
+          </div>
+        </>
+      )}
     </div>
   );
 };
